411ce4925b02b55effbfbac130144589ca7c1253
Jennifer Alexander <jenny_alexander@icloud.com>
2022-04-16 21:58:08 -0500

    app is up - no custom changes yet

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 import App from './components/App/App';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
