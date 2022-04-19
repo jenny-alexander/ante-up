bc2bf2b8b23fc109fa5ca3f2c050c639b6517af3
Jennifer Alexander <jenny_alexander@icloud.com>
2022-04-18 21:46:11 -0500

    Add money table DDL

--- database.sql
+++ database.sql
@@ -4,7 +4,23 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username character varying(80) NOT NULL UNIQUE,
+    password character varying(1000) NOT NULL,
+    dob date
 );
+CREATE TABLE money (
+    id SERIAL PRIMARY KEY,
+    username character varying(80) NOT NULL UNIQUE REFERENCES "user"(username),
+    monthly_total integer,
+    spend_total integer,
+    spend_weekly integer,
+    save_total integer,
+    save_weekly integer,
+    share_total integer,
+    share_weekly integer,
+    spend_weekly_deposited boolean DEFAULT false,
+    save_weekly_deposited boolean DEFAULT false,
+    share_weekly_deposited boolean DEFAULT false
+);
+
