9ff3349430fc1db15a07f6fb952e7a9557c48339
Jennifer Alexander <jenny_alexander@icloud.com>
2022-12-28 15:16:30 -0600

    Commit before starting Chore component

--- src/components/Money/MoneyBucketManager/MoneyBucketManager.jsx
+++ src/components/Money/MoneyBucketManager/MoneyBucketManager.jsx
@@ -21,10 +21,10 @@ function MoneyBucketManager(props) {
         const Toast = Swal.mixin({
             toast: true,
             animation: false,
-            position: 'top',
+            position: 'bottom-left',
             showConfirmButton: false,
             timer: 2000,
-            timerProgressBar: true,
+            //timerProgressBar: true,
           })
           
           Toast.fire({
