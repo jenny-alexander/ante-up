c7c805893b7b18a90843ac9deb88a0e003969990
Jennifer Alexander <jenny_alexander@icloud.com>
2025-01-29 06:31:20 -0700

    Jen test branch (#1)
    * Add word TODAY
    
    * Add dummy exclamation marks
    
    * Add dummy text on welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,9 +7,10 @@ function Welcome(props) {
             <div className="main">
                 <img className="logo" src="images/ante_up_welcome.png" alt="ante up logo"></img>
                 <div className="about-main"> 
+                    <h1>JENNIFER</h1>
                     <div className="about-money">                        
                         <img className="profile-image" src="/images/lottery.png"/>
-                        <div className="about-blurb">Track allowance payments</div>
+                        <div className="about-blurb">Track allowance payments!!!</div>
                     </div>
                     <div className="about-chores">                        
                         <img className="profile-image" src="/images/laundry.png"/>
