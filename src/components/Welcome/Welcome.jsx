86fda8beee94c8df5b53d9892a7ad9526ce7d032
Jennifer Alexander <jenny_alexander@icloud.com>
2025-01-29 07:30:04 -0600

    Add dummy exclamation marks

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -9,7 +9,7 @@ function Welcome(props) {
                 <div className="about-main"> 
                     <div className="about-money">                        
                         <img className="profile-image" src="/images/lottery.png"/>
-                        <div className="about-blurb">Track allowance payments</div>
+                        <div className="about-blurb">Track allowance payments!!!</div>
                     </div>
                     <div className="about-chores">                        
                         <img className="profile-image" src="/images/laundry.png"/>
