60aaebf48de1ad1e108c43c1a9ac1bd9cd56d4ae
Jennifer Alexander <jenny_alexander@icloud.com>
2025-01-29 07:17:30 -0600

    Remove dummy verbiage

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,8 +6,7 @@ function Welcome(props) {
         <div className="welcome">
             <div className="main">
                 <img className="logo" src="images/ante_up_welcome.png" alt="ante up logo"></img>
-                <div className="about-main">
-                    <div>Jennifer's test</div>
+                <div className="about-main"> 
                     <div className="about-money">                        
                         <img className="profile-image" src="/images/lottery.png"/>
                         <div className="about-blurb">Track allowance payments</div>
